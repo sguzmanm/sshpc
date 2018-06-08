10c5905d7d16ba5b80c62733edc046857832645b
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2018-06-08 11:08:52 -0500

    .

--- challenge/matrix.h
+++ challenge/matrix.h
@@ -14,3 +14,8 @@ my_prec **treatMatrix(unsigned int samples, unsigned int size_X, unsigned int si
 double** getOptimalCoefficients (double** x, double** y, int samples,int numVar);
 
 double variance(double** a,double* y,double** x,int N,int n);
+
+// Matrix Transpose Code
+double** Transpose(double** A,int r,int c);
+// Matrix Multiply Code
+double** multiply0(double** a, double** b,int row,int col);
