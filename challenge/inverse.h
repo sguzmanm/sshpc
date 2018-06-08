29e673380098134cfad24f61620128b2f338f63f
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2018-06-08 11:12:37 -0500

    Cambio inverse.h

--- challenge/inverse.h
+++ challenge/inverse.h
@@ -1,3 +1,15 @@
 
 
-void inverse(double** A, double** inverse);
+void getCofactor(double** A, double** temp, int p, int q, int n);
+ 
+/* Recursive function for finding determinant of matrix.
+   n is current dimension of A[][]. */
+int determinant(double** A, int n);
+ 
+// Function to get adjoint of A[N][N] in adj[N][N].
+void adjoint(double** A,double** adj,int N);
+}
+ 
+// Function to calculate and store inverse, returns false if
+// matrix is singular
+void inverse(double** A, double** inverse,int N):
